a634661526dc79e0b503d58fde984845fcdc2a65
jinsujj <wlstncjs1234@naver.com>
2022-11-27 20:31:28 +0900

    3-14 검색

--- pybo/controller/question_views.py
+++ pybo/controller/question_views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, url_for, session, g, flash
 from pybo.forms import AnswerForm, QuestionForm
 
-from pybo.models import Question, User
+from pybo.models import Question, User, Answer, User
 from datetime import datetime
 from werkzeug.utils import redirect
 from logging import Logger
@@ -25,10 +25,34 @@ def load_logged_in_user():
 
 @bp.route("/list/")
 def _list():
-    page = request.args.get("page", type=int, default=1)  # 페이지
+    page = request.args.get("page", type=int, default=1)
+    kw = request.args.get("kw", type=str, default="")
     question_list = Question.query.order_by(Question.create_date.desc())
+
+    if kw:
+        search = "%%{}%%".format(kw)
+        sub_query = (
+            db.session.query(Answer.question_id, Answer.content, User.username)
+            .join(User, Answer.user_id == User.id)
+            .subquery()
+        )
+
+        question_list = (
+            question_list.join(User)
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id)
+            .filter(
+                Question.subject.ilike(search)
+                | Question.content.ilike(search)
+                | User.username.ilike(search)
+                | sub_query.c.content.ilike(search)
+                | sub_query.c.username.ilike(search)
+            )
+            .distinct()
+        )
     question_list = question_list.paginate(page=page, per_page=10)
-    return render_template("question/question_list.html", question_list=question_list)
+    return render_template(
+        "question/question_list.html", question_list=question_list, page=page, kw=kw
+    )
 
 
 @bp.route("/detail/<int:question_id>/")
