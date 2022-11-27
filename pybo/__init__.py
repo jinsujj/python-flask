35fe25bbd4d6d241f0899114e5328c71797493ad
jinsujj <wlstncjs1234@naver.com>
2022-11-27 20:06:48 +0900

    3-13 마크다운 기능 적용하기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flaskext.markdown import Markdown
 import config
 
 db = SQLAlchemy()
@@ -43,4 +44,6 @@ def create_app():
 
     app.jinja_env.filters["datetime"] = format_datetime
 
+    # markdown
+    Markdown(app, extensions=["nl2br", "fenced_code"])
     return app
